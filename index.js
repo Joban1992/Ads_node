76e38bbbd276e245d5bf4c1d08237923ea5bf638
Joban1992 <singh.jobanpreet@tcs.com>
2016-12-22 15:33:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var express =require('express');
-var app = express();
+var app = function () {
+  var http = require('http');
+  var express = require('express');
+  var app = express();
 
+  app.set('views', __dirname + '/');
+  //app.engine('html', require('ejs').renderFile);
+  app.get('/', function(req, res) {
+    res.render('index.html');
+  });
 
+  return app;
+}();
 
-app.use(express.static(__dirname+'/'));
-
-app.get("*", function(req, res){
-  res.sendfile('./index.html');
-})
-
-app.listen(8080);
+module.exports = app;
