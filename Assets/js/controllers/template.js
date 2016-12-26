a2101cd0038ecd09d5616e3d654e2aec2406ab43
unknown <Jobanpreet.Singh@XORIANT-GGN-136.XORIANT.LOCAL>
2016-12-26 16:56:11 +0530

    login files

--- Assets/js/controllers/template.js
+++ Assets/js/controllers/template.js
@@ -1,8 +1,8 @@
 angular.module('app.controllers').controller('templateCntrl', function($scope, $timeout){
 console.log(" $scope.closeMenu");
   $scope.notiText = "";
-   $scope.loginActive = false;
-   $scope.showOverlay = false;
+   $scope.loginActive = true;
+   $scope.showOverlay = true;
   $scope.closeMenu = function(){
      //open menu is in common.js
       $scope.menuActive = false;
