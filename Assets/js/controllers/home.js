8729613dd49a776b8a40c88373338a0644d14604
Joban1992 <singh.jobanpreet@tcs.com>
2016-12-26 16:17:41 +0530

    Update home.js

--- Assets/js/controllers/home.js
+++ Assets/js/controllers/home.js
@@ -3,9 +3,14 @@ angular.module('app.controllers',[]).controller('homeCntrl', function($scope, $t
   
   $scope.init = function(){
     console.log('home init');
-    $scope.menuActive = false;
-	$scope.containerDeactive = false;
+    $scope.$parent.menuActive = false;
+	$scope.$parent.containerDeactive = false ;
+	//$scope.$('overlay')[0].style.display="block";
 	//$timeout(function(){$scope.$parent.notiText = "Home Page Noti"},0);
+  }
+  
+  $scope.$ = function(className){
+     return document.getElementsByClassName(className);
   }
   $scope.init();
 })
