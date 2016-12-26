55abfe5b1d3ec2c996052c1391208e5099270500
Joban1992 <singh.jobanpreet@tcs.com>
2016-12-26 16:18:01 +0530

    Update search.js

--- Assets/js/controllers/search.js
+++ Assets/js/controllers/search.js
@@ -1,4 +1,36 @@
 angular.module('app.controllers').controller('searchCntrl', function($scope, $timeout){
   console.log('search ctrl');
   //$timeout(function(){$scope.$parent.notiText = "Search Page"},0);
+  $scope.searchResultFromServer=[
+    {
+	  id:1,
+	  title:'#1234',
+	  link:"https://www.google.com",
+	  msg:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's."
+    },
+	{
+	  id:2,
+	  title:'#1234',
+	  link:"https://www.google.com",
+	  msg:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's."
+    },
+	{
+	  id:3,
+	  title:'#1234',
+	  link:"https://www.google.com",
+	  msg:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's."
+    },
+	{
+	  id:4,
+	  title:'#1234',
+	  link:"https://www.google.com",
+	  msg:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's."
+    }
+  ];
+  
+  
+  
+  $scope.getResultDetail = function(id){
+    
+  }
 })
