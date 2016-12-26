ca685c411e7bf976d7ae9bc12d867bc48a1e060d
Joban1992 <singh.jobanpreet@tcs.com>
2016-12-26 16:18:18 +0530

    Update template.js

--- Assets/js/controllers/template.js
+++ Assets/js/controllers/template.js
@@ -1,10 +1,13 @@
 angular.module('app.controllers').controller('templateCntrl', function($scope, $timeout){
 console.log(" $scope.closeMenu");
   $scope.notiText = "";
+   $scope.loginActive = false;
+   $scope.showOverlay = false;
   $scope.closeMenu = function(){
      //open menu is in common.js
       $scope.menuActive = false;
 	  $scope.containerDeactive = false;
+	 
   }
   
 })
